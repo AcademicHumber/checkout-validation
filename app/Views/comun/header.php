c754566ba75703a237f11318d4e9532cdb554822
Dariana <darianitamartinezjimenez17@gmail.com>
2021-06-15 19:08:21 -0400

    landing header

--- app/Views/comun/header.php
+++ app/Views/comun/header.php
@@ -6,9 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <link href="css/style.css" rel="stylesheet" type="text/css"> 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-    <title>App Radio Taxi</title>
+    <title>App Checkout</title>
 </head>
 <body style="background-color: #F2F2F2">
     <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #F2F2F2; box-shadow: 2px 2px 7px 5px rgba(0, 0, 0, 0.2);">
